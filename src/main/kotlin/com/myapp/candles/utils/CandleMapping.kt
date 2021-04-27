3b4a0e083f8cae258064cce430b2b5a3cc52baa2
Ioana Sofronia <ioana.sirbu20@gmail.com>
2021-04-27 19:55:54 +0300

    add unit tests for candle service

--- src/main/kotlin/com/myapp/candles/utils/CandleMapping.kt
+++ src/main/kotlin/com/myapp/candles/utils/CandleMapping.kt
@@ -5,11 +5,11 @@ import com.myapp.candles.entities.Candle
 
 class CandleMapping {
 
-    fun dtoToEntity(candleDTO: CandleDTO) : Candle{
+    fun dtoToEntity(candleDTO: CandleDTO): Candle {
         return Candle(scent = candleDTO.scent, price = candleDTO.price, id = candleDTO.id)
     }
 
-    fun entityToDto(candle: Candle) : CandleDTO {
-        return CandleDTO(scent = candle.scent,price = candle.price, id = candle.id)
+    fun entityToDto(candle: Candle): CandleDTO {
+        return CandleDTO(scent = candle.scent, price = candle.price, id = candle.id)
     }
 }
--- src/test/kotlin/com/myapp/candles/services/CandleServiceTest.kt
+++ src/test/kotlin/com/myapp/candles/services/CandleServiceTest.kt
@@ -1,46 +1,94 @@
 package com.myapp.candles.services
 
+import com.myapp.candles.dto.CandleDTO
 import com.myapp.candles.entities.Candle
 import com.myapp.candles.repositories.CandleRepository
-import com.myapp.candles.utils.CandleMapping
 
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.spyk
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
 
 class CandleServiceTest {
 
-//    private val mockRepository: CandleRepository = Mockito.mock(CandleRepository::class.java)
-//    private val candleService: CandleService = CandleService(mockRepository)
+    @MockK
+    private lateinit var mockRepository: CandleRepository
+    private lateinit var candleService: CandleService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        candleService = spyk(CandleService(mockRepository))
+    }
 
     @Test
     fun `When findAll return all` () {
+
         val expected = mutableListOf(Candle("mint", 4.4), Candle("lemon", 7.2))
-//
-//        Mockito.`when`(mockRepository.findAll()).thenReturn(expected.asIterable())
-//
-//        val actual = candleService.findAll();
-//
-//        assertEquals(expected, actual)
+
+        every { mockRepository.findAll() } returns expected
+
+        val actual = candleService.findAll()
+
+        assertEquals(entitiesToDto(expected), actual)
+    }
+
+    @Test
+    fun `When findAll return empty list` () {
+
+        val expected = emptyList<Candle>()
+
+        every { mockRepository.findAll() } returns expected
+
+        val actual = candleService.findAll()
+
+        assertEquals(entitiesToDto(expected), actual)
     }
 
     @Test
     fun `When find by id return the correct object if it exists` () {
-//        val expected = Optional.of(Candle("amber"))
+        val id = UUID.randomUUID()
+        val expected = Optional.of(Candle("amber", 5.5, id))
 
-//        Mockito.`when`(mockRepository.findById(1)).thenReturn(expected)
+        every { mockRepository.findById(id) } returns expected
 
-//        val actual = candleService.findCandle(1)
+        val actual = candleService.findCandle(id)
 
-//        assertEquals(expected, actual)
+        assertEquals(entitiesToDto(mutableListOf(expected.get()))[0], actual)
     }
 
     @Test
-    fun `When find by id return empty if the object does not exist` () {
-//        Mockito.`when`(mockRepository.findById(2)).thenReturn(Optional.empty())
+    fun `When find by id return an error message` () {
+        val id = UUID.randomUUID()
 
-//        val actual = candleService.findCandle(2)
+        every { mockRepository.findById(id) } returns Optional.empty()
 
-//        assert(actual.get().equals(Optional<Candle>.empty()))
+        val actual = candleService.findCandle(id)
+
+        assertEquals("This candle does not exist.", actual)
+    }
+
+    @Test
+    fun `When addCandle return the candle object` () { // TODO fix test
+        val expected = Candle("amber", 5.5)
+
+        every { mockRepository.save(expected) } returns expected
+
+        val actual = candleService.addCandle(entitiesToDto(mutableListOf(expected))[0])
+
+        assertEquals(entitiesToDto(mutableListOf(expected))[0], actual)
+    }
+
+    private fun entitiesToDto(candles: List<Candle>): List<CandleDTO> {
+        var result = mutableListOf<CandleDTO>()
+        for (candle in candles) {
+            result.add(CandleDTO(candle.scent, candle.price, candle.id))
+        }
+
+        return result
     }
 }
